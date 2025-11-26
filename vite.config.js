3f322c7d742066634c97dfcb0104c607db234187
Michael Fienen <fienen@gmail.com>
2025-11-26 10:46:52 -0600

    Change version to alpha

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 const banner = `// ==UserScript==
 // @name         Reedsy Editor Customizations
 // @namespace    http://github.com/fienen
-// @version      1.0.251125
+// @version      0.1.251126
 // @description  Trick Reedsy into giving me dark mode
 // @author       Michael Fienen <fienen@gmail.com>
 // @match        https://editor.reedsy.com/book/*
