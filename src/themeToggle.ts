6abd4ddd2af8f2bffc7d5460ff2b41da4e9ff561
Michael Fienen <fienen@gmail.com>
2025-11-26 18:08:04 -0600

    #5: Wrap up toggle button function and commenting

--- src/themeToggle.ts
+++ src/themeToggle.ts
@@ -1,20 +1,54 @@
+// Set up a reload timer because it can take a few seconds for the editor to load, and we don't want the script to run infinitely if there's a problem.
+let themeButtonReloads = 0;
+
 export const darkLightToggle = () => {
     // Find the theme button wrapper and unbind existing events
-    const defaultBtnWrapper = document.querySelector('[title="Switch theme"]').parentNode;
+    let defaultBtnWrapper = document.querySelector('[title="Switch theme"]');
 
-    if(defaultBtnWrapper && Object.prototype.toString.call(defaultBtnWrapper).includes('HTMLElement')) {
+    // If we didn't find the button, wait and try again
+    if(defaultBtnWrapper) {
+        // Get the button's parent to replace, because that's what Reedsy binds the event to
+        defaultBtnWrapper = defaultBtnWrapper.parentNode as Element | null;
+
+        // If we still don't have it, something went wrong, so exit
+        if(!defaultBtnWrapper) {
+            console.error('[Reedsy Editor Customizations] Failed to find theme button parent. Try reloading the editor.');
+            return;
+        }
+
         // Grab our document root
         const html = document.documentElement;
         const newBtnWrapper = defaultBtnWrapper.cloneNode(true);
+
         newBtnWrapper.addEventListener('click', function () {
             html.classList.toggle('light-theme');
             html.classList.toggle('dark-theme');
+
+            // Save preference for future page loads
+            html.classList.contains('dark-theme') ? GM_setValue('preferredTheme', 'dark') : GM_setValue('preferredTheme', 'light');
         });
+
+        // Replace the old button with the new one
         defaultBtnWrapper.replaceWith(newBtnWrapper);
+    } else {
+        // Increment counter
+        themeButtonReloads++;
+
+        // Stop trying after 10 attempts with appropriate error message
+        if(themeButtonReloads > 10) {
+            console.error('[Reedsy Editor Customizations] Failed to find theme button after multiple attempts. Giving up.');
+            return;
+        } else {
+            console.log('[Reedsy Editor Customizations] Editor not ready, retrying in 5 seconds. (Attempt ' + themeButtonReloads + ' of 10)');
+            setTimeout(darkLightToggle, 5000);
+        }
+        
     }
 };
 
 export const enforceThemePreference = () => {
+    const preferredTheme = GM_getValue('preferredTheme');
+    console.log(`[Reedsy Editor Customizations] Enforcing preferred theme: ${preferredTheme}`);
     /* OLD VERSION FOR REF ONLY
     // Grab our document root
     const html = document.documentElement;
