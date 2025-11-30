0542ed6a3f6603823dfb9690d6906f1299d2dcc6
Michael Fienen <fienen@gmail.com>
2025-11-29 20:21:25 -0600

    #11: Clean up initialization sequence

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
-import { initThemeButton, enforceThemePreference } from './themeToggle';
-import { removeUpsellPanels } from './removeUpsellPanels';
-import { getTopTenWordsWithCounts } from './frequentWords';
+import { initEditorCustomizations } from './utils';
 
 (function() {
     'use strict';
 
-    // Set up an observer to watch for changes to the <html> class list in case Reedsy tries to override our theme preference
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            // This prevents Reedsy from overwriting your change when you navigate chapters
-            if (mutation.attributeName === "class") enforceThemePreference();
-        });
-    });
-
-    // Start observing the <html> element
-    observer.observe(document.documentElement, { attributes: true });
-
     // Wait before trying to initialize to give the editor time to load
-    setTimeout(initThemeButton, 5000);
-    setTimeout(removeUpsellPanels, 5000);
-    setTimeout(getTopTenWordsWithCounts, 5000);
+    setTimeout(initEditorCustomizations, 5000);
 })();
