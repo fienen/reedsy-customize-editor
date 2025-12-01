3f4ff6ea71b32262254c56c778dbcd2eed4d685b
Michael Fienen <fienen@gmail.com>
2025-11-30 18:53:02 -0600

    Meta additions

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-const banner = `// ==UserScript==
-// @name         Reedsy Editor Customizations
-// @namespace    http://github.com/fienen
-// @version      0.5.251130
-// @description  Trick Reedsy into giving me dark mode
-// @author       Michael Fienen <fienen@gmail.com>
-// @match        https://editor.reedsy.com/book/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reedsy.com
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_addStyle
-// @run-at       document-idle
-// ==/UserScript==`;
+const banner = `
+  // ==UserScript==
+  // @name         Reedsy Editor Customizations
+  // @namespace    http://github.com/fienen/
+  // @version      0.5.251130-alpha
+  // @description  Remove upsell banners and features, and enable/add replacement features
+  // @author       Michael Fienen <fienen@gmail.com>
+  // @source       http://github.com/fienen/reedsy-customize-editor/
+  // @match        https://editor.reedsy.com/book/*
+  // @include      https://editor.reedsy.com/book/*
+  // @icon         https://www.google.com/s2/favicons?sz=64&domain=reedsy.com
+  // @grant        GM_setValue
+  // @grant        GM_getValue
+  // @grant        GM_addStyle
+  // @tag          productivity
+  // @supportURL   https://github.com/fienen/reedsy-editor-customizations/issues/
+  // @run-at       document-idle
+  // ==/UserScript==
+`;
 
 export default defineConfig({
   build: {
